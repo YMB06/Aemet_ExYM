85277c9c84077998303c3b50c05ad0acdb538c9e
YMB06 <152322438+YMB06@users.noreply.github.com>
2024-11-22 13:07:23 +0100

    13:07

--- Aemet/src/poo/Aemet_ExYM/AemetYM.java
+++ Aemet/src/poo/Aemet_ExYM/AemetYM.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class AemetYM {
     static int NCIUDADESYM = 8; // Número de ciudades
     static String[] nombresYM = {"Almeria", "Sevilla", "Granada", "Fuengirola", "Cadiz", "Jaen", "Cordoba", "Malaga"};
-    static RegCiudad[] registroTempYM = new RegCiudad[NCIUDADESYM];
+    static RegCiudadYM[] registroTempYM = new RegCiudadYM[NCIUDADESYM];
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -25,13 +25,13 @@ public class AemetYM {
             int tmax = sc.nextInt();
             System.out.print("Introduce la temperatura mínima para " + nombresYM[i] + ": ");
             int tmin = sc.nextInt();
-            registroTempYM[i] = new RegCiudad(nombresYM[i], tmax, tmin);
+            registroTempYM[i] = new RegCiudadYM(nombresYM[i], tmax, tmin);
         }
     }
 
     // Método para listar el registro de temperaturas
     public static void ListarRegistroTempYM() {
-        for (RegCiudad ciudadYM : registroTempYM) {
+        for (RegCiudadYM ciudadYM : registroTempYM) {
             ciudadYM.mostrarTabla();
         }
     }
@@ -41,7 +41,7 @@ public class AemetYM {
         double sumaTempYM = 0; 
         int countYM = 0;
 
-        for (RegCiudad ciudadYM : registroTempYM) {
+        for (RegCiudadYM ciudadYM : registroTempYM) {
             sumaTempYM += ciudadYM.getTempMediaDiaYM(diaYM);
             countYM++;
         }
